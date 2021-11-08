61e6fdb7610b862207c6d4c7953073db081293b8
Loubna Baroudi <loubna.baroudi97@gmail.com>
2021-11-08 11:15:02 -0500

    Add files via upload

--- vicon_dataexport.m
+++ vicon_dataexport.m
@@ -1,31 +1,36 @@
 %% Code copied from Kirsty McDonald's youtube video series
 % https://www.youtube.com/channel/UCIqZ46GRzFbSYNzMVQ5HD-g
 vicon = ViconNexus
-vicon.DisplayCommandList
+vicon.DisplayCommandList()
 vicon.DisplayCommandHelp('CommandGoesHere')
 %% ------------------ Marker Trajectories ----------------
 %%
 % Import the subject name
 SubjectName = vicon.GetSubjectNames;
+%% Import Marker Names for this subject
+vicon.GetMarkerNames('Loubna');
 %%
 % Import RTH1 and RTH3 marker trajectories
 % Input = Subject name, marker name
 % Output = Trajectory data X, Y, Z & E
-[RTH1(:,1),RTH1(:,2),RTH1(:,3),RTH1(:,4)] = vicon.GetTrajectory(SubjectName{1},'RTH1');
-[RTH3(:,1),RTH3(:,2),RTH3(:,3),RTH3(:,4)] = vicon.GetTrajectory(SubjectName{1},'RTH3');
+[RHEE(:,1),RHEE(:,2),RHEE(:,3),RHEE(:,4)] = vicon.GetTrajectory(SubjectName{1},'RHEE');
+[LHEE(:,1),LHEE(:,2),LHEE(:,3),LHEE(:,4)] = vicon.GetTrajectory(SubjectName{1},'LHEE');
+%% PLOT
+figure()
+plot(RHEE)
 %%
 % Create a virtual marker half way between RTH1 and RTH3
-RTHMid = ((RTH1(:,1:3)+RTH3(:,1:3))/2)';
-exists = logical(min(RTH1(:,4),RTH3(:,4)));
+% RTHMid = ((RHEE(:,1:3)+LHEE(:,1:3))/2)';
+% exists = logical(min(RHEE(:,4),LHEE(:,4)));
 %%
 % Put RTHMid virtual marker into the GCS of open trial
-vicon.CreateModelOutput(SubjectName{1},'RTHMid','Modeled Markers',{'X', 'Y', 'Z'}, {'Length', 'Length', 'Length'});
-vicon.SetModelOutput(SubjectName{1},'RTHMid', RTHMid, exists);
+% vicon.CreateModelOutput(SubjectName{1},'RTHMid','Modeled Markers',{'X', 'Y', 'Z'}, {'Length', 'Length', 'Length'});
+% vicon.SetModelOutput(SubjectName{1},'RTHMid', RTHMid, exists);
 %% ---------------- Import model outputs -------------------
 %%
 
 % Import model data for right ankle
-ModelOutput = {'RAnkleAngle', 'RAnkleMoments', 'RPowerAnkle'};
+ModelOutput = {'RAnkleAngles'};
 
 for i = 1:length(ModelOutput)
     [ModelData.Raw.(ModelOutput{i}), ModelData.Exists.(ModelOutput{i})] = vicon.GetModelOutput(SubjectName{1},ModelOutput{i});
@@ -34,13 +39,17 @@ end
 % Import event frames
 Events.RightFS = vicon.GetEvents(SubjectName{1}, 'Right', 'Foot Strike');
 Events.RightFO = vicon.GetEvents(SubjectName{1}, 'Right', 'Foot Off');
-
+%% Plot gait events with trajectory
+figure()
+plot(RHEE)
+hold on
+plot(Events.RightFO,RHEE(Events.RightFO,2),'.')
 % Crop data to stance phase only
-for i = 1:length(ModelOutput)
-    for j = 1:length(Events.RightFO)
-        ModelData.Cropped.(ModelOutput{i}).(strcat('StancePhase',num2str(j)))= ModelData.Raw.(ModelOutput{i})(:,Events.RightFS(j):Events.RightFO(j))';
-    end
-end
+% for i = 1:length(ModelOutput)
+%     for j = 1:length(Events.RightFO)
+%         ModelData.Cropped.(ModelOutput{i}).(strcat('StancePhase',num2str(j)))= ModelData.Raw.(ModelOutput{i})(:,Events.RightFS(j):Events.RightFO(j))';
+%     end
+% end
 %% -------------- Import Forceplates data -------------------
 [sFrame,eFrame] = vicon.GetTrialRegionOfInterest;
 Fs = vicon.GetFrameRate;
@@ -66,14 +75,16 @@ for i = 1:length(DeviceName)
 end
 
 for i = 1:length(DeviceName)
-    if strcmp(DeviceType{i},'ForcePlate');
+    if strcmp(DeviceType{i},'ForcePlate')
         for j = 1:length(Channels)
             ChannelID{j} = vicon.GetDeviceChannelIDFromName(DeviceID{i}, ceil(double(j)/3), Channels{j});
                 % 2nd term is Device OutputID (1 = force, 2 = moment, 3 = CoP)           
             ForcePlateRaw.(DeviceName{i}).(Channels{j}) = vicon.GetDeviceChannel(DeviceID{i}, ceil(double(j)/3), ChannelID{j});            
-            ForcePlateCropped.(DeviceName{i}).(Channels{j}) = ForcePlateRaw.(DeviceName{i}).(Channels{j})(sFrame*(DeviceRate{i}/Fs):eFrame*(DeviceRate{i}/Fs));
+%             ForcePlateCropped.(DeviceName{i}).(Channels{j}) = ForcePlateRaw.(DeviceName{i}).(Channels{j})(sFrame*(DeviceRate{i}/Fs):eFrame*(DeviceRate{i}/Fs));
         end
     end
 end
-
-plot(ForcePlateCropped.Left.Fz);
+%%
+figure()
+plot(abs(ForcePlateRaw.RightForcePlate.Fz))
+% plot(ForcePlateCropped.Left.Fz);
